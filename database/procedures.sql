0528ab463a182052b207f015e854a852213572ac
dbarelop <dbarelop@gmail.com>
2017-07-13 20:29:05 +0200

    Fixed procedure to match students in order to limit the Erasmus number of a peer, partially translated into English

--- database/procedures.sql
+++ database/procedures.sql
@@ -22,98 +22,102 @@ CREATE PROCEDURE emparejar(IN _semester_id INT) BEGIN
     DECLARE _erasmus_id, _erasmus_student_id, _erasmus_studies, _erasmus_faculty INT;
     DECLARE _erasmus_gender_preference, _erasmus_gender, _erasmus_language_preference BOOLEAN;
     DECLARE _erasmus_nationality VARCHAR(2);
-    DECLARE _peer_id, _peer_erasmus_limit, _peer_erasmus_asignados, _peer_student_id, _peer_studies, _peer_faculty INT;
+    DECLARE _peer_id, _peer_erasmus_limit, _peer_student_id, _peer_studies, _peer_faculty INT;
     DECLARE _peer_gender_preference, _peer_aegee_member, _peer_gender, _peer_speaks_english BOOLEAN;
     DECLARE _peer_nationality_preference VARCHAR(2);
-    DECLARE _mejor_peer_id, _max_peso INT;
-    DECLARE _cur_erasmus CURSOR FOR 
+    DECLARE _best_peer_id, _best_rank INT;
+    -- Erasmus without a peer
+    DECLARE _cur_unmatched_erasmus CURSOR FOR
         SELECT ERASMUS.id, ERASMUS.gender_preference, ERASMUS.language_preference, STUDENT.id, STUDENT.gender, STUDENT.nationality, STUDENT.studies, STUDENT.faculty
-        FROM ERASMUS 
-        INNER JOIN STUDENT 
-        ON ERASMUS.erasmus = STUDENT.id 
-        WHERE semester_id = _semester_id AND NOT EXISTS (
-            SELECT * 
-            FROM BUDDY_PAIR 
-            WHERE erasmus = ERASMUS.id) 
+        FROM ERASMUS
+          INNER JOIN STUDENT ON ERASMUS.erasmus = STUDENT.id
+        LEFT JOIN BUDDY_PAIR ON ERASMUS.id = BUDDY_PAIR.erasmus
+        WHERE BUDDY_PAIR.erasmus IS NULL AND semester_id = _semester_id
         ORDER BY register_date ASC;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET _done_erasmus := TRUE;
 
     DROP TABLE IF EXISTS PESOS;
-    CREATE TEMPORARY TABLE PESOS(peso INT DEFAULT 0) 
-        SELECT id AS peer_id, (SELECT COUNT(*) FROM BUDDY_PAIR WHERE peer = peer_id) AS erasmus_asignados 
-        FROM PEER 
-        WHERE semester_id = _semester_id AND (
-            SELECT COUNT(*) 
-            FROM BUDDY_PAIR 
-            WHERE peer = PEER.id) < PEER.erasmus_limit;
-    OPEN _cur_erasmus;
+    CREATE TEMPORARY TABLE RANKS (
+        rank INT DEFAULT 0)
+        SELECT PEER.id AS peer_id, COUNT(BUDDY_PAIR.peer) AS assigned_erasmus
+        FROM PEER
+          LEFT JOIN BUDDY_PAIR ON PEER.id = BUDDY_PAIR.peer
+        WHERE PEER.semester_id = _semester_id
+        GROUP BY PEER.id, PEER.erasmus_limit
+        HAVING COUNT(BUDDY_PAIR.peer) < PEER.erasmus_limit;
+    OPEN _cur_unmatched_erasmus;
     erasmus_loop: LOOP
-        FETCH _cur_erasmus INTO _erasmus_id, _erasmus_gender_preference, _erasmus_language_preference, _erasmus_student_id, _erasmus_gender, _erasmus_nationality, _erasmus_studies, _erasmus_faculty;
+        FETCH _cur_unmatched_erasmus INTO _erasmus_id, _erasmus_gender_preference, _erasmus_language_preference, _erasmus_student_id, _erasmus_gender, _erasmus_nationality, _erasmus_studies, _erasmus_faculty;
         IF _done_erasmus THEN
-            CLOSE _cur_erasmus;
+            CLOSE _cur_unmatched_erasmus;
             LEAVE erasmus_loop;
         END IF;
-        BLOQUE2: BEGIN
-            DECLARE _min_erasmus_asignados INT;
-            DECLARE _cur_peers CURSOR FOR
-                SELECT PEER.id AS peer_id, PEER.gender_preference, PEER.speaks_english, PEER.nationality_preference, PEER.aegee_member, PEER.erasmus_limit, STUDENT.id, STUDENT.gender, STUDENT.studies, STUDENT.faculty 
-                FROM PESOS
-                INNER JOIN PEER 
-                ON PESOS.peer_id = PEER.id
-                INNER JOIN STUDENT 
-                ON PEER.peer = STUDENT.id 
-                WHERE PESOS.erasmus_asignados = _min_erasmus_asignados AND PESOS.erasmus_asignados < PEER.erasmus_limit
+        block2: BEGIN
+            DECLARE _min_assigned_erasmus INT;
+            DECLARE _cur_assignable_peers CURSOR FOR
+                SELECT PEER.id AS peer_id, PEER.gender_preference, PEER.speaks_english, PEER.nationality_preference, PEER.aegee_member, PEER.erasmus_limit, STUDENT.id, STUDENT.gender, STUDENT.studies, STUDENT.faculty
+                FROM RANKS
+                  INNER JOIN PEER ON RANKS.peer_id = PEER.id
+                  INNER JOIN STUDENT ON PEER.peer = STUDENT.id
+                WHERE RANKS.assigned_erasmus = _min_assigned_erasmus
+                      AND RANKS.assigned_erasmus < PEER.erasmus_limit
                 ORDER BY PEER.register_date ASC;
             DECLARE CONTINUE HANDLER FOR NOT FOUND SET _done_peers := TRUE;
 
-            SELECT MIN(erasmus_asignados) INTO _min_erasmus_asignados FROM PESOS;
-            OPEN _cur_peers;
+            SELECT MIN(assigned_erasmus) INTO _min_assigned_erasmus FROM RANKS;
+            OPEN _cur_assignable_peers;
             peers_loop: LOOP
-                FETCH _cur_peers INTO _peer_id, _peer_gender_preference, _peer_speaks_english, _peer_nationality_preference, _peer_aegee_member, _peer_erasmus_limit, _peer_student_id, _peer_gender, _peer_studies, _peer_faculty;
+                FETCH _cur_assignable_peers INTO _peer_id, _peer_gender_preference, _peer_speaks_english, _peer_nationality_preference, _peer_aegee_member, _peer_erasmus_limit, _peer_student_id, _peer_gender, _peer_studies, _peer_faculty;
                 IF _done_peers THEN
                     SET _done_erasmus := FALSE;
                     SET _done_peers := FALSE;
-                    CLOSE _cur_peers;
+                    CLOSE _cur_assignable_peers;
                     LEAVE peers_loop;
                 END IF;
-                -- Mismos estudios -> +2
+                -- Same studies -> +2
                 IF (_erasmus_studies IS NOT NULL) AND (_peer_studies IS NOT NULL) AND (_erasmus_studies = _peer_studies) THEN
-                    UPDATE PESOS SET peso = peso + 2 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank + 2 WHERE peer_id = _peer_id;
                 END IF;
-                -- Misma facultad -> +1; diferente facultad -> -1
+                -- Same faculty -> +1; different faculty -> -1
                 IF (_erasmus_faculty IS NOT NULL) AND (_peer_faculty IS NOT NULL) AND (_erasmus_faculty = _peer_faculty) THEN
-                    UPDATE PESOS SET peso = peso + 1 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank + 1 WHERE peer_id = _peer_id;
                 ELSE
-                    UPDATE PESOS SET peso = peso - 1 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank - 1 WHERE peer_id = _peer_id;
                 END IF;
-                -- Erasmus sin preferencia de género / misma preferencia que tutor -> +1
+                -- Erasmus without gender preference / same preference as peer -> +1
                 IF (_erasmus_gender_preference IS NULL) OR (_erasmus_gender_preference = _peer_gender_preference) THEN
-                    UPDATE PESOS SET peso = peso + 1 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank + 1 WHERE peer_id = _peer_id;
                 END IF;
-                -- Tutor sin preferencia de género / misma preferencia que Erasmus -> +1
+                -- Peer without gender preference / same preference as Erasmus -> +1
                 IF (_peer_gender_preference IS NULL) OR (_peer_gender_preference = _erasmus_gender_preference) THEN
-                    UPDATE PESOS SET peso = peso + 1 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank + 1 WHERE peer_id = _peer_id;
                 END IF;
-                -- Erasmus prefiere inglés pero tutor no habla inglés -> -2
+                -- Erasmus prefers English but peer doesn't speak it -> -2
                 IF (_erasmus_language_preference IS NOT NULL) AND (NOT _erasmus_language_preference AND NOT _peer_speaks_english) THEN
-                    UPDATE PESOS SET peso = peso - 2 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank - 2 WHERE peer_id = _peer_id;
                 END IF;
-                -- Nacionalidad preferida / sin preferencia
+                -- Preferred nationality / no preference
                 IF (_peer_nationality_preference IS NULL) OR (_peer_nationality_preference = _erasmus_nationality) THEN
-                    UPDATE PESOS SET peso = peso + 1 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank + 1 WHERE peer_id = _peer_id;
                 END IF;
-                -- Miembro de AEGEE
+                -- AEGEE member
                 IF (_peer_aegee_member) THEN
-                    UPDATE PESOS SET peso = peso + 1 WHERE peer_id = _peer_id;
+                    UPDATE RANKS SET rank = rank + 1 WHERE peer_id = _peer_id;
                 END IF;
             END LOOP peers_loop;
-        END BLOQUE2;
-        SELECT MAX(peso) INTO _max_peso FROM PESOS;
-        SELECT peer_id INTO _mejor_peer_id FROM PESOS WHERE peso = _max_peso LIMIT 1;
-        CALL emparejar_estudiantes(_erasmus_id, _mejor_peer_id);
-        UPDATE PESOS SET erasmus_asignados = erasmus_asignados + 1 WHERE peer_id = _mejor_peer_id;
-        UPDATE PESOS SET peso = 0;
+        END block2;
+        SELECT MAX(rank)
+        INTO _best_rank
+        FROM RANKS
+          INNER JOIN PEER ON RANKS.peer_id = PEER.id
+        WHERE RANKS.assigned_erasmus < PEER.erasmus_limit;
+        IF (_best_rank IS NOT NULL) THEN
+          SELECT peer_id INTO _best_peer_id FROM RANKS WHERE rank = _best_rank LIMIT 1;
+          CALL emparejar_estudiantes(_erasmus_id, _best_peer_id);
+          UPDATE RANKS SET assigned_erasmus = assigned_erasmus + 1 WHERE peer_id = _best_peer_id;
+        END IF;
+        UPDATE RANKS SET rank = 0;
     END LOOP erasmus_loop;
-    DROP TABLE PESOS;
+    DROP TABLE RANKS;
 END $$
 DELIMITER ;
